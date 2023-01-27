60d7572d25cc1069aad13691d0aa15beab4a1d8d
João Victor Sales <joaovictornascimentosales5@gmail.com>
2023-01-27 08:07:59 -0300

    Mais alterações...
    Adição do while e concertando erros...

--- python/pabd_moveis.py
+++ python/pabd_moveis.py
@@ -2,69 +2,71 @@ import mysql.connector
 
 meubanco = mysql.connector.connect(
     host="localhost",
-    database="moveis",
+    database="crediario_sa",
     user="root",
     password="labinfo"
 )
 
 cursor = meubanco.cursor()
 
-print('1 - Entrar como administrador')
-print('2 - Entrar como vendedor')
-r = int(input('Digite a opção: '))
+while(True):
 
-if r == 1:
-    print('1 - Produtos')
-    print('2 - Funcionários')
-    print('3 - Relatórios')
+    print('1 - Entrar como administrador')
+    print('2 - Entrar como vendedor')
     r = int(input('Digite a opção: '))
 
     if r == 1:
-        print('1 - Cadastrar')
-        print('2 - Alterar')
-        print('3 - Voltar')
+        print('1 - Produtos')
+        print('2 - Funcionários')
+        print('3 - Relatórios')
         r = int(input('Digite a opção: '))
 
         if r == 1:
-            codigo = input(print('Digite o código: '))
-            nome = input(print('Digite o nome: '))
-            tipo = input(print('Digite o tipo: '))
-            descricao = input(print('Digite a descrição: '))
-            valor = input(print('Digite o valor: '))
-            qntd = input(print('Digite a quantidade em estoque: '))
-            cnpj = input(print('Digite o CNPJ do fornecedor: '))
+            print('1 - Cadastrar')
+            print('2 - Alterar')
+            print('3 - Voltar')
+            r = int(input('Digite a opção: '))
 
-            cursor.execute(f"INSERT INTO filme VALUES ('{codigo}', '{nome}', '{tipo}', '{descricao}', '{valor}', '{qntd}', '{cnpj}')")
-            meubanco.commit()
+            if r == 1:
+                codigo = str(input('Digite o código: '))
+                nome = str(input('Digite o nome: '))
+                tipo = str(input('Digite o tipo: '))
+                descricao = str(input('Digite a descrição: '))
+                valor = float(input('Digite o valor: '))
+                qntd = int(input('Digite a quantidade em estoque: '))
+                cnpj = str(input('Digite o CNPJ do fornecedor: '))
 
-        else:
-            print('Comando inválido...')
+                cursor.execute(f"INSERT INTO produto VALUES ('{codigo}', '{nome}', '{tipo}', '{descricao}', '{valor}', '{qntd}', '{cnpj}')")
+                meubanco.commit()
+
+            else:
+                print('Comando inválido...')
     
-    elif r == 2:
-        print('1 - Cadastrar')
-        print('2 - Alterar')
-        print('3 - Voltar')
-        r = int(input('Digite a opção: '))
+        elif r == 2:
+            print('1 - Cadastrar')
+            print('2 - Alterar')
+            print('3 - Voltar')
+            r = int(input('Digite a opção: '))
 
-        if r == 1:
-            print('Digite o cpf: ')
-            print('Digite o nome: ')
-            print('Digite a função: ')
-            print('Digite o valor do salário: ')
-            print('Digite o telefone: ')
-            print('Digite o email: ')
+            if r == 1:
+                cpf = str(input('Digite o cpf: '))
+                nome = str(('Digite o nome: '))
+                funcao = str(('Digite a função: '))
+                salario = str(('Digite o valor do salário: '))
+                telefone = str(('Digite o telefone: '))
+                email = str(('Digite o email: '))
 
-            cursor.execute(f"INSERT INTO filme VALUES ('{codigo}', '{titulo}', '{genero}')")
-            meubanco.commit()
+                cursor.execute(f"INSERT INTO funcionario VALUES ('{cpf}', '{nome}', '{funcao}', '{salario}', '{telefone}', '{email}')")
+                meubanco.commit()
 
-    elif r == 3:
-        print('1 - Lista de vendas por funcionário')
-        print('2 - Lista de vendas por data')
-        print('3 - Lista de vendas por produto')
-        r = int(input('Digite a opção: '))
+        elif r == 3:
+            print('1 - Lista de vendas por funcionário')
+            print('2 - Lista de vendas por data')
+            print('3 - Lista de vendas por produto')
+            r = int(input('Digite a opção: '))
 
-        if r == 1:
-            cursor.execute("SELECT qntd_venda, funcionario.nome FROM venda, funcionario")
+            if r == 1:
+                cursor.execute("SELECT qntd_venda, funcionario.nome FROM venda, funcionario")
 
-            for linha in cursor:
-                print(linha)
+                for linha in cursor:
+                    print(linha)
